b835f44267cc22aad4046f5451be783a462d592c
Wilfred <wilfred05777@gmail.com>
2023-01-20 10:18:29 +0800

    .env variable is not working yet

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,19 @@
 // config/db.js
 const mongoose = require("mongoose");
 // const db = process.env.MONGO_URI;
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 
+// dili jud mo gana pa ni
 const db = process.env.MONGO_URI;
+
 const connectDB = async () => {
   try {
     /// `mongoose.connect()...`  bug not connected to mongodb need to figure this out
+    // dili jud mo gana pa ni
     // await mongoose.connect(
     //   db,
     await mongoose.connect(
+      //// temprary fixed
       `mongodb+srv://merntodoapp:merntodoapp@wilfredcluster.rsfdy.mongodb.net/merntodoapp23?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,

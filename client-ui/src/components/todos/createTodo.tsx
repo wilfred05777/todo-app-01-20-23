685fec81072619252d9c8e5b64f9464d0a44f699
Wilfred <wilfred05777@gmail.com>
2023-01-20 18:48:53 +0800

    fixing the create todo

--- client-ui/src/components/todos/createTodo.tsx
+++ client-ui/src/components/todos/createTodo.tsx
@@ -1,66 +1,73 @@
 import React from "react";
-import {useState} from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import axios from "axios";
 
 const createTodo = () => {
+  const [data, setData] = useState({ title: "", description: "" });
 
-  const[data, setData] = useState({ title: "", description: ""})
+  const handleChange = (e: any) => {
+    setData((data) => ({ ...data, [e.target.name]: e.target.value }));
+  };
 
-  const handleChange =(e:any) => {
-    setData((data) => ({...data, [e.target.name]: e.target.value}))
-  }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
 
-  const handleSubmit = (e:any) =>{
-    e.preventDafault();
-
-    const todo ={
+    const todo = {
       title: data.title,
       description: data.description
-    }
+    };
 
-    console.log({todo})
+    console.log({ todo });
     axios
       .post("http://localhost:8000/api/todo", data)
-      .then((res) =>{
-        setData({title: "", description:""})
-        console.log(res.data.message)
+      .then((res) => {
+        setData({ title: "", description: "" });
+        console.log(res.data.message);
       })
-      .catch((err) =>{
-        console.log("Error couldn't create TODO")
-        console.log(err.message)
-      })
-  }
+      .catch((err) => {
+        console.log("Error couldn't create TODO");
+        console.log(err.message);
+      });
+  };
 
   return (
     <section className="container">
       <Link to="/" className="button-back">
-        <button className="button">
-          back
-        </button>
+        <button className="button">back</button>
       </Link>
       <section className="contents">
-        <form 
-          onSubmit={handleSubmit}
-          className="form-container"
-          noValidate
-        > 
-        <label className="label" htmlFor="title">
-          Title
-        </label>
-        <input type="text" name="title" value={data.title} onChange={handleChange} className="input" />
+        <form onSubmit={handleSubmit} className="form-container" noValidate>
+          <label className="label" htmlFor="title">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            value={data.title}
+            onChange={handleChange}
+            className="input"
+          />
 
-        <label htmlFor="description" className="label">Description</label>
-        <input type="text" className="input" name="description" value={data.description} onChange={handleChange}/>
+          <label htmlFor="description" className="label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="input"
+            name="description"
+            value={data.description}
+            onChange={handleChange}
+          />
 
-        <button type="submit" className="button">
-          create todo
-        </button>
+          <button type="submit" className="button">
+            create todo
+          </button>
         </form>
       </section>
     </section>
-  )
+  );
 };
 
 export default createTodo;
